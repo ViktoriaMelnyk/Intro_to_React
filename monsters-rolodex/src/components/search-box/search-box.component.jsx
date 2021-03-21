090dc34164dab7fd7f66d2a0eaeeb2120a6ee831
ViktoriaMelnyk <oliviatvist12@gmail.com>
2021-03-22 00:31:00 +0100

    done

---
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './search-box.style.css';
+
+export const SearchBox = ({placeholder, hendleChange}) => (
+   <input className="search" type ="search" 
+        placeholder={placeholder} 
+        onChange = {hendleChange}
+    />
+);
+ 
